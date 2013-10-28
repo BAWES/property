df32ceb35a349f4896504773df4424e9773a161a
khalidmnet <khalid@khalidm.net>
2013-10-29 02:54:26 +0300

    Added htaccess and updated config to remove index.php

--- protected/config/main.php
+++ protected/config/main.php
@@ -40,6 +40,7 @@ return array(
 		
 		'urlManager'=>array(
 			'urlFormat'=>'path',
+                        'showScriptName'=>false,
 			'rules'=>array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
