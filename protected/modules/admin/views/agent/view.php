ea63c3ca2bee5e884078e008ecf301921f869155
khalidmnet <khalid@khalidm.net>
2013-10-31 13:08:56 +0300

    Display passwords as ***

--- protected/modules/admin/views/agent/view.php
+++ protected/modules/admin/views/agent/view.php
@@ -2,27 +2,34 @@
 /* @var $this AgentController */
 /* @var $model Agent */
 
-$this->breadcrumbs=array(
-	'Agents'=>array('index'),
-	$model->agent_name,
+$this->breadcrumbs = array(
+    'Agents' => array('index'),
+    $model->agent_name,
 );
 
-$this->menu=array(
-	array('label'=>'List Agents', 'url'=>array('index')),
-	array('label'=>'Create Agent', 'url'=>array('create')),
-	array('label'=>'Update Agent', 'url'=>array('update', 'id'=>$model->agent_id)),
-	array('label'=>'Delete Agent', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->agent_id),'confirm'=>'Are you sure you want to delete this item?')),
+$this->menu = array(
+    array('label' => 'List Agents', 'url' => array('index')),
+    array('label' => 'Create Agent', 'url' => array('create')),
+    array('label' => 'Update Agent', 'url' => array('update', 'id' => $model->agent_id)),
+    array('label' => 'Delete Agent', 'url' => '#', 'linkOptions' => array('submit' => array('delete', 'id' => $model->agent_id), 'confirm' => 'Are you sure you want to delete this item?')),
 );
 ?>
 
 <h1>View Agent <?php echo $model->agent_name; ?></h1>
 
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		//'agent_id',
-		'agent_name',
-		'agent_email',
-		'agent_password',
-	),
-)); ?>
+<?php
+$this->widget('zii.widgets.CDetailView', array(
+    'data' => $model,
+    'attributes' => array(
+        //'agent_id',
+        'agent_name',
+        'agent_email',
+        //'agent_password',
+        array(// related city displayed as a link
+            'label' => 'Agent Password',
+            'type' => 'raw',
+            'value' => "***",
+        ),
+    ),
+));
+?>
--- protected/modules/admin/views/client/view.php
+++ protected/modules/admin/views/client/view.php
@@ -2,27 +2,34 @@
 /* @var $this ClientController */
 /* @var $model Client */
 
-$this->breadcrumbs=array(
-	'Clients'=>array('index'),
-	$model->client_name,
+$this->breadcrumbs = array(
+    'Clients' => array('index'),
+    $model->client_name,
 );
 
-$this->menu=array(
-	array('label'=>'List Clients', 'url'=>array('index')),
-	array('label'=>'Create Client', 'url'=>array('create')),
-	array('label'=>'Update Client', 'url'=>array('update', 'id'=>$model->client_id)),
-	array('label'=>'Delete Client', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->client_id),'confirm'=>'Are you sure you want to delete this item?')),
+$this->menu = array(
+    array('label' => 'List Clients', 'url' => array('index')),
+    array('label' => 'Create Client', 'url' => array('create')),
+    array('label' => 'Update Client', 'url' => array('update', 'id' => $model->client_id)),
+    array('label' => 'Delete Client', 'url' => '#', 'linkOptions' => array('submit' => array('delete', 'id' => $model->client_id), 'confirm' => 'Are you sure you want to delete this item?')),
 );
 ?>
 
 <h1>View Client <?php echo $model->client_name; ?></h1>
 
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		//'client_id',
-		'client_name',
-		'client_email',
-		'client_password',
-	),
-)); ?>
+<?php
+$this->widget('zii.widgets.CDetailView', array(
+    'data' => $model,
+    'attributes' => array(
+        //'client_id',
+        'client_name',
+        'client_email',
+        array(// related city displayed as a link
+            'label' => 'Client Password',
+            'type' => 'raw',
+            'value' => "***",
+        ),
+        //'client_password',
+    ),
+));
+?>
